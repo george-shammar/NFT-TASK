1ab006bbf26109653e08c8cb73a57b59e47472e9
george-shammar <george.gbenle@outlook.com>
2021-10-19 10:25:54 +0000

    Import Minter inside App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './Nav';
+import Minter from './Minter';
 
 const App = () => {
   return (
     <div>
      <Nav />
+     <Minter />
     </div>
   );
 }
