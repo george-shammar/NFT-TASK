247fda53f3746b1bc6e66d00fff3c0ba5928c9c7
george-shammar <george.gbenle@outlook.com>
2021-10-19 05:46:04 +0000

    Create state

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { useState, useEffect } from "react";
 
 const Nav = () => {
+
+    const [ethAdd, setEthAdd] = useState(null);
+    const [metaMaskStatus, setMetaMaskStatus] = useState(true);
+    
     return (
-        
-    )
+        <div>
+            <h1>task</h1>
+        </div>
+    );
 }
 
 export default Nav;
