abb17d0d84ebfc513cde44cf09a3e0283915103e
george-shammar <george.gbenle@outlook.com>
2021-10-19 08:03:09 +0000

    Refator nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,48 +9,10 @@ let web3 = window.web3;
 
 const Nav = () => {
 
-    const [ethAdd, setEthAdd] = useState(null);
-    const [metaMaskStatus, setMetaMaskStatus] = useState(true);
-
-    const checkWallet = async () => {
-        const accounts = await window.web3.eth.getAccounts();
-        return accounts.length > 0;
-    };
-
-    const connectWallet = async () => {
-        const userAdd = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setEthAdd(userAdd[0]);
-    };
-
-    useEffect(() => {
-        (async () => {
-            if (window.ethereum) {
-                window.web3 = new Web3(window.ethereum);
-                if (await checkWallet()) {
-                  connectWallet();
-                }
-              } else {
-                setMetaMaskStatus(false);
-              }
-        })()
-        
-    }, []);
-
     return (
         <div>
-            <h1>task</h1>
+        <h1>task</h1>
 
-        <button>
-        <div onClick={() => metaMaskStatus ? ethAdd ? null : connectWallet : () => window.open("https://metamask.io/")}>
-          {metaMaskStatus
-            ? ethAdd
-              ? "Connected: " + truncate(ethAdd, 13)
-              : "Connect Wallet"
-            : "Install MetaMask 🦊"}
-        </div>
-      </button>
         </div>
     );
 }
