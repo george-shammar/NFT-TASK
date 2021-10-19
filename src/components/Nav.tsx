acfc83e24b6e716eee1d27f44e5b4c8e58057a0d
george-shammar <george.gbenle@outlook.com>
2021-10-19 05:48:06 +0000

    Implement checkWallet

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+import Web3 from "web3";
 
 const Nav = () => {
 
     const [ethAdd, setEthAdd] = useState(null);
     const [metaMaskStatus, setMetaMaskStatus] = useState(true);
-    
+
+    const checkWallet = async () => {
+        const accounts = await window.web3.eth.getAccounts();
+        return accounts.length > 0;
+    };
+
     return (
         <div>
             <h1>task</h1>
