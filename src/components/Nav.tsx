b6540ad61478f4e605314eec160a8234406e454d
george-shammar <george.gbenle@outlook.com>
2021-10-19 08:10:08 +0000

    Set state

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import { connectWallet } from "../utils/wallet";
 
-declare const window: any;
-declare var truncate: any
+// declare const window: any;
+// declare var truncate: any
 
-let web3 = window.web3;
+// let web3 = window.web3;
 
 const Nav = () => {
-
+    const [walletAddress, setWallet] = useState("");
+    const [status, setStatus] = useState("");
     return (
         <div>
         <h1>task</h1>
