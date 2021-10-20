fb1b115a4b1f9711873e1fa339bd6cc2a189ef21
george-shammar <george.gbenle@outlook.com>
2021-10-20 13:04:19 +0000

    Add display flex for form content

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -5,10 +5,14 @@ const Minter = () => {
         <div className="fadeInDown minter">
             <h2>Mint NFT</h2>
             <div className="formContent">
-                <label>Recipient's Address</label>
-                <input /> <br />
-                <label>No of NFTs</label>
-                <input /><br />
+                <div className="flex">
+                    <label>Recipient's Address:</label>
+                    <input />
+                </div>
+                <div className="flex">
+                    <label>No of NFTs:</label>
+                    <input />
+                </div>
                 <button className="confirm-text">Confirm</button>
             </div>
         </div>
