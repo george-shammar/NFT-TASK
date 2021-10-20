01b6360ef615ebd7eb3eb3b32cc5870d15203d4b
george-shammar <george.gbenle@outlook.com>
2021-10-20 12:19:33 +0000

    Mint NFT

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -3,10 +3,11 @@ import "../styles/minter.css";
 const Minter = () => {
     return (
         <div className="minter">
-            <h2>Mint Task NFT</h2>
+            <h2>Mint NFT</h2>
             <form>
-                
+                <label>Recipient's Address</label>
                 <input />
+                <label>No of NFTs</label>
                 <input />
                 <button>Confirm</button>
             </form>
