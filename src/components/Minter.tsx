35bd15c2d3f3585f80598d1a10991b11b64fbbed
george-shammar <george.gbenle@outlook.com>
2021-10-20 12:14:36 +0000

    Refactor jsx input

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -5,8 +5,9 @@ const Minter = () => {
         <div className="minter">
             <h2>Mint Task NFT</h2>
             <form>
-                <input>Address</input>
-                <input>No of NFT</input>
+                
+                <input />
+                <input />
                 <button>Confirm</button>
             </form>
         </div>
