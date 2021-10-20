ffcb388fd1d49a11dc90784dafd4a129cb2cebbb
george-shammar <george.gbenle@outlook.com>
2021-10-20 12:59:30 +0000

    Style minter

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -4,7 +4,7 @@ const Minter = () => {
     return (
         <div className="fadeInDown minter">
             <h2>Mint NFT</h2>
-            <div>
+            <div className="formContent">
                 <label>Recipient's Address</label>
                 <input /> <br />
                 <label>No of NFTs</label>
