45a71fa5eb46121ef040510cb87d1be01f3b5bc3
george-shammar <george.gbenle@outlook.com>
2021-10-22 00:52:51 +0000

    Use standard library

--- contracts/Zebra.sol
+++ contracts/Zebra.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Zebra is ERC721, Pausable, Ownable {
+contract Zebra is ERC721URIStorage, Pausable, Ownable {
 
     constructor()ERC721("Zebra", "ZBR"){
 
