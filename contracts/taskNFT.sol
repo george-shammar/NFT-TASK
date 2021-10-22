d1f0427871341fcf69b186d1038913273c3d2b00
george-shammar <george.gbenle@outlook.com>
2021-10-22 00:45:52 +0000

    Import standard libraries

--- contracts/taskNFT.sol
+++ contracts/taskNFT.sol
@@ -2,4 +2,8 @@
 
 pragma solidity ^0.8.9;
 
-
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
