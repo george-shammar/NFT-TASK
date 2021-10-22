7c1eba2bcf9ea46ca4a34df1c996181ed3b30710
george-shammar <george.gbenle@outlook.com>
2021-10-22 00:52:06 +0000

    Add standard library

--- contracts/Zebra.sol
+++ contracts/Zebra.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Zebra is ERC721, Pausable, Ownable {
 
