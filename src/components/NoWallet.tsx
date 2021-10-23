e7447e07acf9ee6a0b4fb05c06fc24589e479633
george-shammar <george.gbenle@outlook.com>
2021-10-23 19:57:42 +0000

    Implement NoWallet

---
+++ src/components/NoWallet.tsx
@@ -0,0 +1,22 @@
+export function NoWalletDetected() {
+    return (
+      <div>
+        <div>
+          <div>
+            <p>
+              No Ethereum wallet was detected. <br />
+              Please install{" "}
+              <a
+                href="http://metamask.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                MetaMask
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
