b953cc2609e45c3f9c8b9bd396edfcc94771d278
george-shammar <george.gbenle@outlook.com>
2021-10-23 19:24:20 +0000

    Implement saveFrontendFiles

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,6 +13,27 @@ async function main() {
     //  To save the contract's artifacts and address in the frontend directory
     saveFrontendFiles(zebra);
   }
+
+  function saveFrontendFiles(zebra) {
+    const fs = require("fs");
+    const contractsDir = __dirname + "../src/contracts";
+  
+    if (!fs.existsSync(contractsDir)) {
+      fs.mkdirSync(contractsDir);
+    }
+  
+    fs.writeFileSync(
+      contractsDir + "/contract-address.json",
+      JSON.stringify({ Zebra: zebra.address }, undefined, 2)
+    );
+  
+    const ZebraArtifact = artifacts.readArtifactSync("Zebra");
+  
+    fs.writeFileSync(
+      contractsDir + "/Zebra.json",
+      JSON.stringify(ZebraArtifact, null, 2)
+    );
+  }
   
   main()
     .then(() => process.exit(0))
