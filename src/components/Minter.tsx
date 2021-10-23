02fc7c8f8907c2c837490e29f977ab1e70a63294
george-shammar <george.gbenle@outlook.com>
2021-10-23 21:34:45 +0000

    Declare signer

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -11,6 +11,8 @@ const Minter = () => {
         const web3Modal = new Web3Modal();
         const connection = await web3Modal.connect();
         const provider = new ethers.providers.Web3Provider(connection);
+
+        const signer = provider.getSigner();
     }
 
     return (
