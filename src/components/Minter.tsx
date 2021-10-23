5edbd37efefe88901fa250f3c7210b9b69c9cc34
george-shammar <george.gbenle@outlook.com>
2021-10-23 23:50:44 +0000

    Made successful transaction

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -1,21 +1,23 @@
 import { ethers } from "ethers";
-import Web3Modal from "web3modal";
-import { useState } from "react";
+// import Web3Modal from "web3modal";
+// import { useState } from "react";
 // import contractAddress from "../contracts/contract-address.json";
 import { ZebraArtifact } from "../contracts/Zebra";
 import "../styles/minter.css";
-const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
-
+declare let window:any;
 const Minter = () => {
+    // const [formInput, updateFormInput] = useState({uri:"", no:""});
 
     async function mintZebra(uri, no) {
-        
-        const web3Modal = new Web3Modal();
-        const connection = await web3Modal.connect();
+        uri = "https://codedli.com";
+        no = 2;
+        // const web3Modal = new Web3Modal();
+        // const connection = await web3Modal.connect();
        
-        const provider = new ethers.providers.Web3Provider(connection);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
 
         const signer = provider.getSigner();
         
@@ -25,7 +27,7 @@ const Minter = () => {
         try {
             const transaction = await contract.createToken(uri, no);
             const receipt = await transaction.wait();
-            
+            console.log("Successful");
             if (receipt.status === 0) {
                 throw new Error("Transaction failed");
             }
