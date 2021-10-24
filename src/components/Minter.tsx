82b74ea686d804aee312f6a10caa2f7b2675ef6b
george-shammar <george.gbenle@outlook.com>
2021-10-24 01:42:51 +0000

    Set file url

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -25,6 +25,8 @@ const Minter = () => {
                     progress: (prog) => console.log(`received: ${prog}`)
                 }
             )
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`
+            setFileUrl(url);
         } catch (e) {
             console.log(e);
         }
