b0c7b7847fba6e4d1d79c2dfe4c26bca2c257ccc
george-shammar <george.gbenle@outlook.com>
2021-10-25 14:56:49 +0000

    Update image style

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
 
-// const ALCHEMY_API_KEY = process.env.KEY;
-// ROPSTEN_PRIVATE_KEY = process.env.PRIVATE_KEY
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
 module.exports = {
   solidity: "0.8.4",
-  // networks: {
-  //   ropsten: {
-  //     url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-  //     accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
-  //   }
-  // }
+ 
 };
