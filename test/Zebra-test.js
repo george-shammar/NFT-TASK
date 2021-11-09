ce9341f47339db6438b5ae2fccb5d7f7e328ee0f
george-shammar <george.gbenle@outlook.com>
2021-11-09 18:18:58 +0000

    Re-organize test suite

--- test/Zebra-test.js
+++ test/Zebra-test.js
@@ -14,7 +14,6 @@ describe("Zebra NFT", () => {
     });
    
     it("Should initialize Zebra contract", async () => {
-        // @ts-ignore
         const ZebraFactory = await ethers.getContractFactory("Zebra");
 
         const zebraContract = await ZebraFactory.deploy();
@@ -23,6 +22,28 @@ describe("Zebra NFT", () => {
 
     it("Should set the right owner", async () => {
         expect(await zebraContract.owner()).to.equal(await owner.address);
+    });
+
+    it("Should confirm that an address is whitelisted and mint token", async () => {
+        [account1] = await ethers.getSigners();
+        const whitelistedAddresses = [];
+        whitelistedAddresses.push(account1.address);
+        await zebraContract.whitelistUsers(whitelistedAddresses);
+        const uri = "https://zebra";
+        const tx = await zebraContract.connect(account1).createToken(uri, 1,
+            {value: zebraContract.getMintingPrice(1)});
+        expect(await zebraContract.balanceOf(account1.address)).to.equal(1);
+    });
+
+    it("Should mint token properly when whitelisting period is over", async () => {
+        [account1] = await ethers.getSigners();
+        expect(await zebraContract.balanceOf(account1.address)).to.equal(0);
+        await zebraContract.setOnlyWhitelisted(false);
+        const uri = "https://zebra";
+        const tx = await zebraContract.connect(account1).createToken(uri, 1,
+            {value: zebraContract.getMintingPrice(1)});
+        expect(await zebraContract.balanceOf(account1.address)).to.equal(1);
+
     });
 
     it("Should mint a zebra", async () => {
@@ -35,38 +56,5 @@ describe("Zebra NFT", () => {
         );
     });
 
-    it("Should mint token properly", async () => {
-        [account1] = await ethers.getSigners();
-        expect(await zebraContract.balanceOf(account1.address)).to.equal(0);
-        await zebraContract.setOnlyWhitelisted(false);
-        const uri = "https://zebra";
-        const tx = await zebraContract.connect(account1).createToken(uri, 1,
-            {value: zebraContract.getMintingPrice(1)});
-        expect(await zebraContract.balanceOf(account1.address)).to.equal(1);
-
-    });
-
-    it("Should confirm that an address is whitelisted", async () => {
-        [account1] = await ethers.getSigners();
-        const whitelistedAddresses = [];
-        whitelistedAddresses.push(account1.address);
-        await zebraContract.whitelistUsers(whitelistedAddresses);
-        const uri = "https://zebra";
-        const tx = await zebraContract.connect(account1).createToken(uri, 1,
-            {value: zebraContract.getMintingPrice(1)});
-        expect(await zebraContract.balanceOf(account1.address)).to.equal(1);
-    });
-
-    it("Should throw an error if an address is NOT whitelisted", async () => {
-        [account1] = await ethers.getSigners();
-        await zebraContract.setOnlyWhitelisted(false);
-        // const whitelistedAddresses = [];
-        // whitelistedAddresses.push(account1.address);
-        // await zebraContract.whitelistUsers(whitelistedAddresses);
-        const uri = "https://zebra";
-        expect(await zebraContract.connect(account1).createToken(uri, 1,
-            {value: zebraContract.getMintingPrice(1)})).to.throw();
-        // expect(await zebraContract.balanceOf(account1.address)).to.equal(1);
-    })
     
 });
