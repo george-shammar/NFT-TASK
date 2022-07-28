8b91743bbb98b2dc3a8fd6a12b850cef00bff332
george-shammer <george.gbenle@outlook.com>
2022-07-28 13:22:06 +0100

    Delete old dependency graph

--- old/contracts/Zebra.sol
+++ old/contracts/Zebra.sol
@@ -2,13 +2,6 @@
 
 pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
 import "openzeppelin-token/ERC721.sol";
 import "openzeppelin-security/Pausable.sol";
 import "openzeppelin-access/Ownable.sol";
