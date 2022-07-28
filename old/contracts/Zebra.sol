a8ea0c8f6c48eadc3435dad8d3ec08c0ed33a9a5
george-shammer <george.gbenle@outlook.com>
2022-07-28 13:12:16 +0100

    Update remappings

--- old/contracts/Zebra.sol
+++ old/contracts/Zebra.sol
@@ -9,6 +9,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
 /// @title A basic ERC721 smart contract that allows user to mint multiple tokens at once.  
 /// @dev Inherits standard Openzeppelin's library.
 
